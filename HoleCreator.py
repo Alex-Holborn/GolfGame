5325ee99475dd234e2498ad64676135adda48415
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-11-11 00:32:09 +1000

    add get_hole add create_distance

--- HoleCreator.py
+++ HoleCreator.py
@@ -1,2 +1,21 @@
+import Hole
+import random
+
 class HoleCreator:
-    pass
+
+    def __init__(self, number, par):
+        self.hole = Hole.Hole()
+        self.hole.set_number(number)
+        self.hole.set_par(par)
+        self.hole.set_distance(self.create_distance(par))
+
+    def get_hole(self):
+        return self.hole
+
+    def create_distance(self, par):
+        if par == 3:
+            return random.randint(220, 255)
+        if par == 4:
+            return random.randint(260, 450)
+        if par == 5:
+            return random.randint(455, 690)
