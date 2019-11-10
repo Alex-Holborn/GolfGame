1466344258c7905ee209d2ba64da09da82518bcc
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-11-11 00:31:50 +1000

    add set_par add set_distance add set_number add __str__

--- Hole.py
+++ Hole.py
@@ -1,2 +1,18 @@
 class Hole:
-    pass
+
+    def __init__(self):
+        self.number = 0
+        self.distance = 0
+        self.par = 0
+
+    def set_par(self, par):
+        self.par = par
+
+    def set_distance(self, dist):
+        self.distance = dist
+
+    def set_number(self, number):
+        self.number = number
+
+    def __str__(self):
+        return "Hole #{0} Par {1} Distance {2} Yards".format(self.number, self.par, self.distance)
