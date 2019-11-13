e60ba8a9c3bafa3dd88930a1fb9f70d14be52302
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-11-13 16:39:46 +1000

    add play_hole

--- Hole.py
+++ Hole.py
@@ -1,9 +1,12 @@
 class Hole:
 
-    def __init__(self):
+    def __init__(self, course):
+        self.course = course
         self.number = 0
         self.distance = 0
         self.par = 0
+        self.has_been_played = False
+        self.shots_taken = 0
 
     def set_par(self, par):
         self.par = par
@@ -13,6 +16,9 @@ class Hole:
 
     def set_number(self, number):
         self.number = number
+
+    def play_hole(self):
+        print(self)
 
     def __str__(self):
         return "Hole #{0} Par {1} Distance {2} Yards".format(self.number, self.par, self.distance)
