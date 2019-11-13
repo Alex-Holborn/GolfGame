f0bf1e20c308a72ee44ce4683fc35e0e9b2c7c30
Alex-Holborn <alexander.holborn@my.jcu.edu.au>
2019-11-13 16:38:17 +1000

    add complete_hole add play_next_hole add get_hole_by_number add play_course

--- Course.py
+++ Course.py
@@ -3,8 +3,22 @@ class Course:
     def __init__(self):
         self.holes = []
         self.current_hole = 0
+        self.shots_taken = 0
         print("Course Created.")
 
     def add_hole(self, hole):
         self.holes.append(hole)
         pass
+
+    def get_hole_by_number(self, number):
+        return self.holes[number - 1]
+
+    def complete_hole(self):
+        self.current_hole += 1
+
+    def play_next_hole(self):
+        self.holes[self.current_hole].play_hole()
+
+    def play_course(self):
+        print("Let's play golf!")
+        self.play_next_hole()
